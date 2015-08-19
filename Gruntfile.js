8fbd69cfe48e8efd35d26b67c2d634d4975c7a7f
Charlie Schliesser <charlie.schliesser@gmail.com>
2015-08-19 14:43:07 -0500

    Legacy blog articles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,56 @@
+"use strict";
+
+var _ = require('lodash');
+var path = require('path');
+
 module.exports = function(grunt) {
+
+    function titleToFilename(string) {
+        var filename = string.toLowerCase();
+        filename = filename.replace('"', '');
+        filename = filename.replace("'", '');
+        filename = filename.replace(/\W+/g, ' ');
+        filename = filename.trim();
+        filename = filename.replace(/ /g, '-');
+        return filename;
+    }
+
+    function createPages(datasrc, template) {
+        var data = grunt.file.readJSON(datasrc);
+        var pageArray = [];
+
+        data.forEach(function(post) {
+
+            var filename = titleToFilename(post.title);
+
+            pageArray.push({
+                filename: filename,
+                data: post,
+                content: grunt.file.read(template)
+            });
+        });
+
+        return _.flatten(pageArray);
+    }
+
+    function createLegacyBlogListing(datasrc, template) {
+        var data = grunt.file.readJSON(datasrc);
+        var pageArray = [];
+
+        // Generate filenames.
+        data.forEach(function(post, i) {
+            data[i].filename = titleToFilename(post.title);
+        });
+
+        pageArray.push({
+            filename: 'index.html',
+            data: {posts: data},
+            content: grunt.file.read(template)
+        });
+
+        return _.flatten(pageArray);
+    }
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -25,6 +77,24 @@ module.exports = function(grunt) {
                     src: ['**/*.hbs', '!**/index.hbs' ],
                     dest: 'dist/',
                     ext: '/index.html',
+                }]
+            },
+            legacy_blog_posts: {
+                options: {
+                    pages: createPages('source/legacy-blog/data.json', 'source/legacy-blog/post.hbs')
+                },
+                files: [{
+                    dest: 'dist/legacy-blog/',
+                    src: '!*'
+                }]
+            },
+            legacy_blog_index: {
+                options: {
+                    pages: createLegacyBlogListing('source/legacy-blog/data.json', 'source/legacy-blog/index.hbs')
+                },
+                files: [{
+                    dest: 'dist/legacy-blog/',
+                    src: '!*'
                 }]
             }
         },
